b65c26a97dd332dfc28c62dd77a18e95f0155695
Unknown <Dannyrhaki@gmail.com>
2018-10-26 13:29:13 -0400

    Sent question to advice
    Used URL to store ID, used ID to pull question in advice page. JSON can be really annoying.

--- api/Advice.php
+++ api/Advice.php
@@ -2,17 +2,21 @@
     include "db.php";
     header('Content-Type: application/json');
     $Question_ID = $_POST["Question_ID"];
-
-    $result = $con->query("SELECT * FROM Questions WHERE Question_ID='$Question_ID'");
-    if($result->num_rows == 0){
-        $output = "Doesnt Work!";
+    
+    $dataquery = mysqli_query($con, "SELECT * FROM Questions WHERE Question_ID='$Question_ID'");
+    
+    if($dataquery->num_rows == 0){
+        $output = "Question not found!";
         
         echo json_encode($output);
         die();
     }
     else{
-        $output = "Works!";
-        echo json_encode($output);
+        $arr = array();
+        while($r = mysqli_fetch_object($dataquery)){
+            array_push($arr, array("Question_ID" => $r->Question_ID, "Description" => $r->Description, "Subject" => $r->Subject, "topic" => $r->topic, "question_type" => $r->question_type));
+        }
+        echo json_encode($arr);
         die();
     }
 
