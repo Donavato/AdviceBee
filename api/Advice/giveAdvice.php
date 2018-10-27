70e4fb05d6223f922d139228ab70a25a873b98db
Unknown <Dannyrhaki@gmail.com>
2018-10-27 15:39:45 -0400

    Forgot to save user ID in advices woops

--- api/Advice/giveAdvice.php
+++ api/Advice/giveAdvice.php
@@ -4,17 +4,17 @@
     
     $advice = $_POST["advice"];
     $Question_ID = $_POST["Question_ID"];
-    $dataquery = mysqli_query($con,"INSERT INTO `advice` (`advice`,`question_id`) VALUES ('$advice','$Question_ID')");
+    $uID = $_SESSION['user_ID'];
+    $dataquery = mysqli_query($con,"INSERT INTO `advice` (`advice`,`question_id`, `user_id`) VALUES ('$advice','$Question_ID', '$uID')");
     
     if($dataquery){
         echo json_encode("success");
         die();
     }
     else{
-        
+        echo(mysqli_error($con));
         echo json_encode("error");
         die();
-        echo(mysqli_error($con));
     }
 
 ?>
