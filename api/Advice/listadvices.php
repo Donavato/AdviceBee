ee2390df63175c25cc1a4d30c57cb8ba1643a584
Pemching <mastervipergts@gmail.com>
2018-10-28 15:44:24 -0400

    edited changepw and listing question
    change implementation for listing username, and added error checking for change password

--- api/Advice/listadvices.php
+++ api/Advice/listadvices.php
@@ -3,18 +3,24 @@
     header('Content-Type: application/json');
     $Question_ID = $_POST["Question_ID"];
 
-    //GRAB THE USER FIRST NAME AND LAST NAME
-    $uNAME = $_SESSION['f_name'];
-    $lNAME = $_SESSION['l_name'];
-
-    //UPDATES USER NAME COLUMN FOR ADVICE TABLE GIVEN IN THE DATABASE
-    mysqli_query($con, "UPDATE `advice` SET f_name='$uNAME', l_name='$lNAME' WHERE question_id='$Question_ID'");
-
     //QUERY TO SELECT ALL FROM THE ADVICE TABLE WITH SPECIFIC QUESTION ID
-    $dataquery = mysqli_query($con, "SELECT * FROM advice WHERE question_id='$Question_ID'");
+    $datastring = mysqli_query($con, "SELECT * FROM advice WHERE question_id='$Question_ID'");
     $arr = array();
-    while($r = mysqli_fetch_object($dataquery)){
-        array_push($arr, array("advice" => $r->advice, "f_name" => $r->f_name, "l_name" => $r->l_name));
+
+    while($r = mysqli_fetch_object($datastring)){
+        $user_ID = $r->user_id;
+        $advice = $r->advice;
+
+        $query = mysqli_query($con, "SELECT f_name, l_name FROM users WHERE user_ID='$user_ID'");
+
+        while($z = mysqli_fetch_object($query)){
+            $fname = $z->f_name;
+            $lname = $z->l_name;
+            $Name = $fname . ' ' . $lname;
+            array_push($arr, array("advice" => $advice, "name" => $Name));
+        }
+        
     }
     echo json_encode($arr);
+    die();
 ?>
