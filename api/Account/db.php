94e95b44ec411e8b1070800bd1ddfc10bb951ca0
Unknown <Dannyrhaki@gmail.com>
2018-11-01 20:34:27 -0400

    SWEET BABY JESUS
    Images are uploaded and displayed from DB. it only took 9 hours. tried to convert to BLOB but no luck after a long time. just used string.

--- api/Account/db.php
+++ api/Account/db.php
@@ -2,6 +2,6 @@
 //allows for cross platform access
 header('Access-Control-Allow-Origin: *');
 //connection string to the database, ([databaseip],[user],[password],[databasename])
-$con = mysqli_connect("localhost","root","","AdviceBee") or die ("could not connect database");
+$con = mysqli_connect("10.0.2.2","root","","AdviceBee") or die ("could not connect database");
 session_start();
 ?>
