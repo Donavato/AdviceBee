1050a681a409c8e16e8b42ba9bf3038d1d6bcf7b
Aaron <fm5962@wayne.edu>
2018-11-13 14:32:55 -0500

    UI changes
    Added icons to buttons. logout, filter button image, cleaned up image storage

--- api/Question/fetchdata.php
+++ api/Question/fetchdata.php
@@ -1,8 +1,14 @@
 <?php
     include "../Account/db.php";
     header('Content-type: application/json');
-    $dataquery = mysqli_query($con, "SELECT * FROM Questions");
+    $dataquery = mysqli_query($con, "SELECT *, (select count(*) from advice where advice.question_id = questions.Question_ID) AS CommentCount FROM Questions");
+    //mysqli_query($con, "SELECT * FROM Questions");
     $arr = array();
+
+    //mysqli_query($con, "SELECT *, (select count(*) from advice where advice.question_id = questions.Question_ID) AS CommentCount FROM Questions");
+    // while($r = mysqli_fetch_object($dataquery)){
+    // $comment_count = $r->CommentCount;
+    // }
 
     while($r = mysqli_fetch_object($dataquery)){
         $Question_ID = $r->Question_ID;
@@ -12,6 +18,8 @@
         $hide = $r->hide;
         $user_ID2 = $r->user_id;
         $d_Image = $r->image;
+        $comment_count = $r->CommentCount;
+
 
         if($d_Image == NULL){
             $d_Image = NULL;
@@ -44,7 +52,7 @@
 
                     array_push($arr, array("Question_ID" => $Question_ID, "Description" => $Description, 
                     "Subject" => $Subject, "anonymous" => $anonymous, "hide" => $hide, "user_ID2" => $user_ID2, 
-                    "name" => $Name, "pImage" => "<img src = $p_Image>", "dImage" => "<img src = $d_Image>"));
+                    "name" => $Name, "pImage" => "<img src = $p_Image>", "dImage" => "<img src = $d_Image>", "c_count" => $comment_count));
 
                 }
 
